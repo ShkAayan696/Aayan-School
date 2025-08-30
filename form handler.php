45eac8e9db0b8ff9c064495e6c1b3ceb09c23dd7
ShkAayan696 <157953810+ShkAayan696@users.noreply.github.com>
2025-08-30 15:42:41 +0530

    Add files via upload

---
+++ form handler.php
@@ -0,0 +1,27 @@
+<?php
+$name=$_POST['name'];
+$visitors_email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+$email_from='info@aayanschool.com';
+
+$email_subject='New Form Submission';
+
+$email_body="User Name:$name.\n".
+            "User Name:$visitor_email.\n".
+            "User Name:$subject.\n".
+            "User Name:$message.\n";
+
+
+
+
+$to='Shaikhaayan801@gmail.com';
+
+$headers='From $email_from \r\n';
+$headers.=Reply-To:$visitor_email \r\n;
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("location:contact.html");
+?>
